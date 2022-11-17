0d5ae8b5103b1d44f5f211d31c976a3d22126e4c
MinhTruong <minhcm2@gmail.com>
2022-11-17 16:40:01 +0700

    change terraform_remote_state in zone2/_data.tf

--- starter-code/zone2/_data.tf
+++ starter-code/zone2/_data.tf
@@ -19,7 +19,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3" 
   config = {
-     bucket = "project-2-sre-course-toanld7"
+     bucket = "udacity-project-2-sre-minh"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
