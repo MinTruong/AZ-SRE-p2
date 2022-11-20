7938a30e2d035d7b17e7081f4d280b31e74be7b5
MinhTruong <minhcm2@gmail.com>
2022-11-20 16:43:21 +0700

    add template and modify ec2 image

--- starter-code/zone2/ec2.tf
+++ starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-015a92b8cba4bc45a"
+   aws_ami            = "ami-0506b6d1a17c92868"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
