16e279509aeb12061d3d6ef606c62eb968198191
MinhTruong <minhcm2@gmail.com>
2022-11-20 18:23:28 +0700

    limited ec2 = 2

--- starter-code/zone2/main.tf
+++ starter-code/zone2/main.tf
@@ -2,12 +2,13 @@ locals {
    account_id = data.aws_caller_identity.current.account_id
 
    name   = "udacity"
+   namespace = "monitoring"
    region = "us-west-1"
    tags = {
      Name      = local.name
      Terraform = "true"
    }
-   namespace = "monitoring"
+   
  }
 
 #  module "vpc" {
